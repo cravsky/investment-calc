7be855efa4918264741586b1e34500d007066dea
cravsky <cravsky@gmail.com>
2024-04-09 10:27:36 +0200

    Making UserInput Interactive

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,24 +1,48 @@
+import { useState } from "react"
+
 export default function UserInput() {
+
+    const [initialInvestment, setInitialInvestment] = useState(10000);
+    const [annualInvestment, setAnnualInvestment] = useState(1200);
+    const [expectedReturn, setExpectedReturn] = useState(7);
+    const [duration, setDuration] = useState(12);
+
+    const handleInitialInvestmentChange = (event) => {
+        setInitialInvestment(event.target.value)
+    }
+
+    const handleAnnualInvestmentChange = (event) => {
+        setAnnualInvestment(event.target.value)
+    }
+
+    const handleExpectedReturnChange = (event) => {
+        setExpectedReturn(event.target.value)
+    }
+
+    const handleDurationChange = (event) => {
+        setDuration(event.target.value)
+    }
+
     return (
         <section id="user-input">
             <div className="input-group">
                 <div>
                     <label htmlFor="initial-investment">Initial Investment</label>
-                    <input type="number" id="initial-investment" value="10000" />
+                    <input type="number" id="initial-investment" onChange={handleInitialInvestmentChange} value={initialInvestment} />
                 </div>
                 <div>
                     <label htmlFor="annual-investment">Annual Investment</label>
-                    <input type="number" id="annual-investment" value="1200" />
+                    <input type="number" id="annual-investment" onChange={handleAnnualInvestmentChange} value={annualInvestment} />
                 </div>
             </div>
             <div className="input-group">
                 <div>
                     <label htmlFor="initial-investment">Expected Return</label>
-                    <input type="number" id="expected-return" value="7" />
+                    <input type="number" id="expected-return" onChange={handleExpectedReturnChange} value={expectedReturn} />
                 </div>
                 <div>
                     <label htmlFor="initial-investment">Duration</label>
-                    <input type="number" id="duration" value="12" />
+                    <input type="number" id="duration" onChange={handleDurationChange} value={duration} />
                 </div>
             </div>
         </section>
