0e3018650b86ca06fde5612123545b85d60762f4
cravsky <cravsky@gmail.com>
2024-04-11 15:36:17 +0200

    Introduced dummy data

---
+++ src/components/ResultsTable/dummyData.js
@@ -0,0 +1,16 @@
+export const dummyAnnualData = [
+    {
+        year: 1, // Year
+        investmentValue: 10850, // Investment Value
+        interestEarnedInYear: 550, // Interest (Year)
+        interest: 550, // Total Interest
+        annualInvestment: 10300 // Invested Capital
+    },
+    {
+        year: 2,
+        investmentValue: 11747,
+        interestEarnedInYear: 597,
+        interest: 1147,
+        annualInvestment: 10600
+    },
+]
