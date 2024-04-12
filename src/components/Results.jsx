3d12f4a9db75f2896d487ebfbe8b1e5a8903dcb3
cravsky <cravsky@gmail.com>
2024-04-12 10:54:47 +0200

    Results table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,48 @@
-import {calculateInvestmentResults} from '../util/investment.js'
+import { calculateInvestmentResults } from '../util/investment.js'
+import { formatter } from '../util/investment.js'
+
 export default function Results({ input }) {
 
     const results = calculateInvestmentResults(input)
-    console.log(results)
-    
+    const initialInvestment = 
+        results[0].valueEndOfYear -
+        results[0].interest -
+        results[0].annualInvestment // calulating missing table data
+
     return (
-        
-            <h2>Results</h2>
-           
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {results.map((annualData) => {
+                    // calculating missing data
+                    const totalInterest =
+                        annualData.valueEndOfYear -
+                        annualData.annualInvestment * annualData.year -
+                        initialInvestment;
+
+                    const totalAmountInvested = annualData.valueEndOfYear - totalInterest;
+
+                    return (
+                        <tr key={annualData.year}>
+                            <td>{annualData.year}</td>
+                            <td>{formatter.format(annualData.valueEndOfYear)}</td>
+                            <td>{formatter.format(annualData.interest)}</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+
+
     );
 }
